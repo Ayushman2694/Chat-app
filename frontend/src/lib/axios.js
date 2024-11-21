c2f8c510d963c151f5383bdd3b2caf60fbfec3b6
Ayushman2694 <sahuayushman4@gmail.com>
2024-11-21 20:29:24 +0530

    changing backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:4500/api",
+    baseURL:"https://chat-app-mdn1.onrender.com/api",
     withCredentials:true
 })
